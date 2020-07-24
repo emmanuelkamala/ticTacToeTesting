6f19057381af4f89bbf681bfa0bc9603b73da0b8
emmanuelkamala <emmanuelkamala86@gmail.com>
2020-07-24 07:36:57 +0300

    Add test for the player module

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,6 +1,16 @@
-const playerFactory = require('../src/Player');
+import { Player } from '../src/player';
 
-test('Create Player names and their turn to play', () => {
-    let testPerson = playerFactory('John', 'X')
-    expect(testPerson).toEqual({name: 'John', mark: 'X'});
+describe('Player', () => {
+  let player;
+  beforeEach(() => {
+    player = new Player();
   });
+
+  it('has a player - x - when initialized', () => {
+    expect(player.player1).toEqual('x');
+  });
+
+  it('has a player - o - when initialized', () => {
+    expect(player.player2).toEqual('o');
+  });
+});
